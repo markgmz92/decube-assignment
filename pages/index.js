3790ff6c7ac8619ba1463679106bc63a444ad92b
markgmz92 <markgmz92@gmail.com>
2022-10-06 15:41:55 +0800

    Added a button with logic and styling

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
+import React, {useState} from 'react'
 
 export default function Home() {
+  const [counter, setCounter] = useState(0)
+
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  }
+
+  const decrementCounter = () => {
+    if(counter !== 0) {
+      setCounter(counter - 1);
+    }
+  }
   return (
     <div>
       <h1>Hello World</h1>
+      <div className='counter-div'>
+        <button type='button' onClick={incrementCounter}>Increment</button>
+        <button type='button' onClick={decrementCounter}>Decrement</button>
+        <h1>{counter}</h1>
+      </div>
+
     </div>
   )
 }
